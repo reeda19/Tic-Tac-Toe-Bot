191b5393e0b4b05fa70b40c536294d1b1eac144d
Alex Reed <alexreed100@gmail.com>
2019-05-15 20:32:26 -0400

    Added input restriction

--- Tic Tac Toe Bot/Tic_Tac_Toe_Bot.py
+++ Tic Tac Toe Bot/Tic_Tac_Toe_Bot.py
@@ -23,7 +23,7 @@ def check_for_two(x, o):
             return vacancy_col
         elif(count_row>=2 and not row_x):
             return vacancy_row
-    return None
+    return []
 
 def check_corners():
     if(elements[0][0]=='X' and elements[2][2]=='X') or (elements[0][2]=='X' and elements[2][0]=='X'):
@@ -35,24 +35,24 @@ def check_corners():
             return [1, 2]
         elif elements[2][1]==' ':
             return [2, 1]
-    return None
+    return []
 def computer_move():
     win = check_for_two('O','X')
     corners = check_corners()
-    if not win == None:
+    if len(win):
         elements[win[0]][win[1]] = 'O'
         return
     elif elements[1][1] == ' ':
         elements[1][1] = 'O'
         return
     two = check_for_two('X','O')
-    if not two == None:
+    if len(two)>0:
         elements[two[0]][two[1]] = 'O'
         return
     elif elements[1][1] == ' ':
         elements[1][1] = 'O'
         return
-    if not check_corners == None:
+    if len(corners)>0:
         elements[corners[0]][corners[1]] = 'O'
 
 elements = [[' ', ' ', ' '],[' ', ' ', ' '], [' ', ' ', ' ']]
@@ -66,6 +66,17 @@ while True:
      |   |
      """).format(elements[0][0],elements[0][1],elements[0][2],elements[1][0],elements[1][1],elements[1][2],elements[2][0],elements[2][1],elements[2][2])
     print(grid)
-    user_input=int(input('Where would you like to go?'))
-    elements[int(user_input/3)][int(user_input%3)]='X'
+    while True:
+        try:
+            user_input=int(input('Where would you like to go: '))
+            if user_input<0 or user_input>8: #out of bounds
+               print('Please enter a number between 0 and 8 inclusive')
+            elif not elements[int(user_input/3)][int(user_input%3)]== ' ':
+               print('Please choose an open space')
+            else:
+             elements[int(user_input/3)][int(user_input%3)]='X'
+             break
+        except ValueError: #not an integer
+            print('Please enter a valid integer')
+      
     computer_move()
