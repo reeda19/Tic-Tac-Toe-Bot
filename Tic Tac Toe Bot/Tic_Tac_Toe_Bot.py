c147325aef28e06aa8433a1d86138fc7a8626ab9
Alex Reed <alexreed100@gmail.com>
2019-05-15 21:56:30 -0400

    Refactored elements array to be 1d

--- Tic Tac Toe Bot/Tic_Tac_Toe_Bot.py
+++ Tic Tac Toe Bot/Tic_Tac_Toe_Bot.py
@@ -1,61 +1,76 @@
+import sys
 def check_for_two(x, o):
-    for r in range (3):
+
+     #checking rows
+    for i in range (0, 7, 3):
         count_row = 0
-        count_column = 0
         row_x  = False
-        col_x = False
         vacancy_row=None
-        vacancy_col=None
-        for c in range(3):
-            if elements[r][c]==x:
-                count_row+=1
-            elif elements[r][c]==o:
-                row_x = True
-            else:
-                vacancy_row=[r, c]
-            if elements[c][r]==x:
-                count_column+=1
-            elif elements[c][r]==o:
-                col_x = True
-            else:
-                vacancy_col=[c, r]
-        if(count_column>=2 and not col_x):
-            return vacancy_col
-        elif(count_row>=2 and not row_x):
+        for j in range (i,i+3):
+            if elements[j]==x:
+              count_row+=1
+            elif not elements[j] == o:
+                vacancy_row = j
+        if(count_row>=2 and vacancy_row is not None):
             return vacancy_row
-    return []
+
+    #checking columns
+    for i in range (3):
+        vacancy_col=None
+        count_column = 0
+        col_x = False
+        for j in range (i, i+7, 3):
+            if elements[j] == x:
+                count_column += 1
+            elif not elements[j] == o:
+                vacancy_col = j
+        if(count_column>=2 and vacancy_col is not None):
+            return vacancy_col
+    return -1
 
 def check_corners():
-    if(elements[0][0]=='X' and elements[2][2]=='X') or (elements[0][2]=='X' and elements[2][0]=='X'):
-        if elements[0][1]==' ':
-            return [0, 1]
-        elif elements[1][0]==' ':
-            return [1, 0]
-        elif elements[1][2]==' ':
-            return [1, 2]
-        elif elements[2][1]==' ':
-            return [2, 1]
-    return []
+    if(elements[0]=='X' and elements[8]=='X') or (elements[2]=='X' and elements[6]=='X'):
+        if elements[1]==' ':
+            return 1
+        elif elements[3]==' ':
+            return 3
+        elif elements[5]==' ':
+            return 5
+        elif elements[7]==' ':
+            return 7
+    return -1
+
+def go_random():
+    try:
+        return elements.index(' ')
+    except ValueError:
+        print('It\'s a tie!')
+        #exit here
+
 def computer_move():
     win = check_for_two('O','X')
     corners = check_corners()
-    if len(win):
-        elements[win[0]][win[1]] = 'O'
+    if win>=0:
+        elements[win] = 'O'
+        #print('win')
         return
-    elif elements[1][1] == ' ':
-        elements[1][1] = 'O'
+    elif elements[4] == ' ':
+        elements[4] = 'O'
         return
     two = check_for_two('X','O')
-    if len(two)>0:
-        elements[two[0]][two[1]] = 'O'
+    if two>=0:
+        elements[two] = 'O'
+        #print('two')
         return
-    elif elements[1][1] == ' ':
-        elements[1][1] = 'O'
+    if corners>=0:
+        elements[corners] = 'O'
+        #print('corners')
         return
-    if len(corners)>0:
-        elements[corners[0]][corners[1]] = 'O'
+    elements[go_random()] = 'O'
+    #print('random')
+    return
 
-elements = [[' ', ' ', ' '],[' ', ' ', ' '], [' ', ' ', ' ']]
+elements = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 while True:
     grid = ("""
    {} | {} | {}
@@ -64,17 +79,17 @@ while True:
   ___|___|___
    {} | {} | {}
      |   |
-     """).format(elements[0][0],elements[0][1],elements[0][2],elements[1][0],elements[1][1],elements[1][2],elements[2][0],elements[2][1],elements[2][2])
+     """).format(elements[0],elements[1],elements[2],elements[3],elements[4],elements[5],elements[6],elements[7],elements[8])
     print(grid)
     while True:
         try:
             user_input=int(input('Where would you like to go: '))
             if user_input<0 or user_input>8: #out of bounds
                print('Please enter a number between 0 and 8 inclusive')
-            elif not elements[int(user_input/3)][int(user_input%3)]== ' ':
+            elif not elements[user_input]== ' ':
                print('Please choose an open space')
             else:
-             elements[int(user_input/3)][int(user_input%3)]='X'
+             elements[user_input]='X'
              break
         except ValueError: #not an integer
             print('Please enter a valid integer')
