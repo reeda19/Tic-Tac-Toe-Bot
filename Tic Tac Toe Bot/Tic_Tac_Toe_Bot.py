aeb5bd1135416b073380ed0d039ac4b7a909b21b
Alex Reed <alexreed100@gmail.com>
2019-09-12 22:56:02 -0400

    Added comments

--- Tic Tac Toe Bot/Tic_Tac_Toe_Bot.py
+++ Tic Tac Toe Bot/Tic_Tac_Toe_Bot.py
@@ -1,5 +1,6 @@
 import sys
-did_win = False
+computer_win = False #global variable that checks if computer has won
+
 def check_for_two(x, o):
      #checking rows
     for i in range (0, 7, 3):
@@ -28,8 +29,10 @@ def check_for_two(x, o):
             return vacancy_col
     return -1
 
+#checks if there is an 'x' in 2 opposite corners. If so, returns a middle space on the outer ring as a countermove
 def check_corners():
     if(elements[0]=='X' and elements[8]=='X') or (elements[2]=='X' and elements[6]=='X'):
+        #if space is empty, return that space
         if elements[1]==' ':
             return 1
         elif elements[3]==' ':
@@ -39,61 +42,76 @@ def check_corners():
         elif elements[7]==' ':
             return 7
     return -1
-
+#computer moves in a random position
 def go_random():
     try:
         return elements.index(' ')
     except ValueError:
         print('It\'s a tie!')
         #exit here
-
+#places on 'O' for the computer
 def computer_move():
+
+    #check for two O's in a row
     win = check_for_two('O','X')
-    corners = check_corners()
     if win>=0:
        elements[win] = 'O'
-       global did_win
-       did_win = True
+       global computer_win
+       computer_win = True
        return
+
+    #check for corners
+    corners = check_corners()
+    if corners>=0:
+        elements[corners] = 'O'
+        return
+    
+    #If center square is open, go there
     elif elements[4] == ' ':
         elements[4] = 'O'
         return
+
+
     two = check_for_two('X','O')
     if two>=0:
         elements[two] = 'O'
         return
-    if corners>=0:
-        elements[corners] = 'O'
-        return
+
+
+    #Go random if no other moves are open
     elements[go_random()] = 'O'
     return
 
 elements = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+#continuously executed until game is over. Main function
 while True:
+    #Tic tac toe grid that is formatted using elements of an array
     grid = ("""
    {} | {} | {}
   ___|___|___
-   {} | {} | {}
+   {} | {} | {} 
   ___|___|___
    {} | {} | {}
      |   |
      """).format(elements[0],elements[1],elements[2],elements[3],elements[4],elements[5],elements[6],elements[7],elements[8])
     print(grid)
 
-    if did_win:
+    #checks if computer has 3 in a row
+    if computer_win:
         print("Computer has won!")
         exit(0)
+    #repeats until player correctly executes a move
     while True:
         try:
             user_input=int(input('Where would you like to go: '))
             if user_input<0 or user_input>8: #out of bounds
                print('Please enter a number between 0 and 8 inclusive')
-            elif not elements[user_input]== ' ':
+            elif not elements[user_input]== ' ': #space is not empty
                print('Please choose an open space')
             else:
-             elements[user_input]='X'
+             elements[user_input]='X' #place X in user position if available
              break
         except ValueError: #not an integer
             print('Please enter a valid integer')
-      
+    #computer's turn
     computer_move()
